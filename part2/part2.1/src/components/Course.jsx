600654bb2f7d3f031fbd5c1889ec59a17de8e9f6
Yannan Zhang <zhangyannan199744@outlook.com>
2024-09-24 12:39:29 +0300

    part2.1

---
+++ part2/part2.1/src/components/Course.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <h1>{course.name}</h1>
+            <div>
+                {course.parts.map(part =>
+                    <p key={part.id}>
+                        {part.name} {part.exercises}
+                    </p>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Course
